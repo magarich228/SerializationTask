7f1646b3400e3f3035ba9c61a11d37c598705892
Kirill <kirillgroshev52@gmail.com>
2023-06-21 16:19:31 +0300

    Инициализация проекта, добавление моделей.

---
+++ SerializationTask/Models/Child.cs
@@ -0,0 +1,17 @@
+﻿namespace SerializationTask.Models
+{
+    public class Child
+    {
+        public Int32 Id { get; set; }
+        public String FirstName { get; set; } = null!;
+        public String LastName { get; set; } = null!;
+        public Int64 BirthDate { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ SerializationTask/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace SerializationTask.Models
+{
+    public class Person
+    {
+        public Int32 Id { get; set; }
+        public Guid TransportId { get; set; }
+        public String FirstName { get; set; } = null!;
+        public String LastName { get; set; } = null!;
+        public Int32 SequenceId { get; set; }
+        public String[] CreditCardNumbers { get; set; } = null!;
+        public Int32 Age { get; set; }
+        public String[] Phones { get; set; } = null!;
+        public Int64 BirthDate { get; set; }
+        public Double Salary { get; set; }
+        public Boolean IsMarred { get; set; }
+        public Gender Gender { get; set; }
+        public Child[] Children { get; set; } = null!;
+    }
+}
